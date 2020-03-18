884badb622c4bc41a6d21d2647f60ef388b4f5f1
Exucounter <exucounter777@gmail.com>
2020-03-18 19:38:48 +0200

    Added tabs

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-let data1 = 'apple';
-const data2 = data1;
-var data3 = 'apple';
+let chatTabs = document.querySelectorAll('.chat-tab');
+let welcomeChatHeading = document.querySelector('.welcome-chat .lg-heading');
+
+chatTabs.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        if(!(item.classList.contains('active')) && item.classList.contains('admin')){
+            chatTabs.forEach((item)=>{
+                item.classList.remove('active'); 
+            });
+            item.classList.add('active');
+            welcomeChatHeading.innerHTML = `
+                Добро пожаловать в автоматически<br> отслеживаемые чаты!
+            `
+        }
+        if(!(item.classList.contains('active')) && !(item.classList.contains('admin'))){
+            chatTabs.forEach((item)=>{
+                item.classList.remove('active'); 
+            });
+            item.classList.add('active');
+            welcomeChatHeading.innerHTML = `
+                   Добро пожаловать
+            `
+        }
+    });
+})
