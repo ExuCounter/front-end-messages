c9703815d40751ec2c30d995cb0f6d50588131f6
Exucounter <exucounter777@gmail.com>
2020-03-19 00:04:12 +0200

    Upd

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let chatTabs = document.querySelectorAll('.chat-tab');
 let welcomeChatHeading = document.querySelector('.welcome-chat .lg-heading');
+let allContactsBtn = document.querySelector('.all-contacts-btn');
+let welcomeChat = document.querySelector('.welcome-chat');
+let allContactsScreen = document.querySelector('.all-contacts');
+let chatRightColumn = document.querySelector('.chat-right-col');
 
 chatTabs.forEach((item)=>{
     item.addEventListener('click', ()=>{
         chatTabs.forEach((item)=>{
             item.classList.remove('active'); 
         });
-
         if(!(item.classList.contains('active')) && item.classList.contains('admin')){
             item.classList.add('active');
             welcomeChatHeading.innerHTML = `Добро пожаловать в автоматически<br> отслеживаемые чаты!`
@@ -17,3 +20,16 @@ chatTabs.forEach((item)=>{
         }
     });
 })
+
+allContactsBtn.addEventListener('click', ()=>{
+    if(allContactsBtn.classList.contains('active')){
+        allContactsBtn.classList.remove('active');
+        allContactsScreen.classList.remove('active');
+        welcomeChat.classList.add('active');
+    }
+    else{
+        allContactsBtn.classList.add('active');
+        allContactsScreen.classList.add('active');
+        welcomeChat.classList.remove('active');
+    }
+});
