7363702f9cff2ff1cd9df52dd53cc92d7cb1e5dc
Exucounter <exucounter777@gmail.com>
2020-03-20 11:11:17 +0200

    chat upd

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ let addUsersBtn = document.querySelector('.chat--create-btn');
 let leftColMessages = document.querySelectorAll('.left-col-message');
 let finishCreateChatBtn = document.querySelector('.finish-create-chat--btn');
 let chatScreen = document.querySelector('.chat');
+let sendMessageCol = document.querySelector('.send-messages-col');
+let chatFooterTextarea = document.querySelector('.chat-footer textarea');
+
+sendMessageCol.addEventListener('click', ()=>{
+    chatFooterTextarea.focus();
+})
 
 finishCreateChatBtn.addEventListener('click', ()=>{
     activeScreen(screens, 4);
@@ -154,3 +160,51 @@ cancelCreateChatButtons.forEach((btn)=>{
 addUsersBtn.addEventListener('click', ()=>{
     activeScreen(screens, 3);
 });
+
+
+/* TEXTAREA AUTORESIZE */
+
+let hiddenDiv = document.createElement('div'),
+    content = null;
+
+// Adds a class to all textareas
+chatFooterTextarea.classList.add('txtstuff');
+
+hiddenDiv.classList.add('txta');
+hiddenDiv.style.display = 'none';
+hiddenDiv.style.whiteSpace = 'pre-wrap';
+hiddenDiv.style.wordWrap = 'break-word';
+
+    chatFooterTextarea.addEventListener('input', function() {
+      
+      // Append hiddendiv to parent of textarea, so the size is correct
+      chatFooterTextarea.parentNode.appendChild(hiddenDiv);
+      
+      // Remove this if you want the user to be able to resize it in modern browsers
+      chatFooterTextarea.style.resize = 'none';
+      
+      // This removes scrollbars
+      chatFooterTextarea.style.overflow = 'hidden';
+
+      // Every input/change, grab the content
+      content = chatFooterTextarea.value;
+
+      // Add the same content to the hidden div
+      
+      // This is for old IE
+      content = content.replace(/\n/g, '<br>');
+      
+      // The <br ..> part is for old IE
+      // This also fixes the jumpy way the textarea grows if line-height isn't included
+      hiddenDiv.innerHTML = content + '<br style="line-height: 3px;">';
+
+      // Briefly make the hidden div block but invisible
+      // This is in order to read the height
+      hiddenDiv.style.visibility = 'hidden';
+      hiddenDiv.style.display = 'block';
+      chatFooterTextarea.style.height = hiddenDiv.offsetHeight + 'px';
+
+      // Make the hidden div display:none again
+      hiddenDiv.style.visibility = 'visible';
+      hiddenDiv.style.display = 'none';
+    });
