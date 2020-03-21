6585c752ba890a7f7dcce7a2703b4270eb190294
Exucounter <exucounter777@gmail.com>
2020-03-21 13:24:14 +0200

    upd

--- script.js
+++ script.js
@@ -1,38 +1,121 @@
-let chatTabs = document.querySelectorAll('.chat-tab');
-let welcomeChatHeading = document.querySelector('.welcome-chat .lg-heading');
-let allContactsBtn = document.querySelector('.all-contacts-btn');
-let welcomeChatScreen = document.querySelector('.welcome-chat');
-let allContactsScreen = document.querySelector('.all-contacts');
-let chatRightColumn = document.querySelector('.chat-right-col');
-let chatLeftColumn = document.querySelector('.chat-left-col');
-let createChatButtons = document.querySelectorAll('.create-chat-btn');
-let newChatScreen = document.querySelector('.new-chat-start');
-let cancelCreateChatButtons = document.querySelectorAll('.cancel-chat-btn');
-let contactHeadCheckBoxes = document.querySelectorAll('.contact-head-label input');
-let contactHeadSubCheckBoxes = document.querySelectorAll('.parent-checkbox-sub input');
-let addUsersScreen = document.querySelector('.add-users');
-let addUsersBtn = document.querySelector('.chat--create-btn');
-let leftColMessages = document.querySelectorAll('.left-col-message');
-let finishCreateChatBtn = document.querySelector('.finish-create-chat--btn');
-let chatScreen = document.querySelector('.chat');
-let sendMessageCol = document.querySelector('.send-messages-col');
-let chatFooterSubmitCol = document.querySelector('.submit-col');
-let showInfoButtons = document.querySelectorAll('.show-info-button');
-let privateScreen = document.querySelector('.private-chat');
-let testChat = document.querySelector('.chat-1');
-let testChatCommon = document.querySelector('.chat-2');
-let commonChatScreen = document.querySelector('.common-chat');
-let seenMessagesScreen = document.querySelector('.seen-messages-screen');
-let messageInfoButtons = document.querySelectorAll('.common-chat .message-info-icon');
-let commonChatSettingsScreen = document.querySelector('.common-chat-settings');
-let chatSettingsButtons = document.querySelectorAll('.chat-settings-btn');
-let pushSettingsBtn = document.querySelector('.push-settings--btn');
+let welcomeChatScreen = document.querySelector('.welcome-chat'),
+    allContactsScreen = document.querySelector('.all-contacts'),
+    commonChatSettingsScreen = document.querySelector('.common-chat-settings'),
+    privateScreen = document.querySelector('.private-chat'),
+    chatScreen = document.querySelector('.chat'),
+    commonChatScreen = document.querySelector('.common-chat'),
+    seenMessagesScreen = document.querySelector('.seen-messages-screen'),
+    newChatScreen = document.querySelector('.new-chat-start'),
+    addUsersScreen = document.querySelector('.add-users');
+
+let cancelCreateChatButtons = document.querySelectorAll('.cancel-chat-btn'),
+    allContactsBtn = document.querySelector('.all-contacts-btn'),
+    addUsersBtn = document.querySelector('.chat--create-btn'),
+    showInfoButtons = document.querySelectorAll('.show-info-button'),
+    chatSettingsButtons = document.querySelectorAll('.chat-settings-btn'),
+    pushSettingsBtn = document.querySelector('.push-settings--btn'),
+    createChatButtons = document.querySelectorAll('.create-chat-btn'),
+    finishCreateChatBtn = document.querySelector('.finish-create-chat--btn'),
+    messageInfoButtons = document.querySelectorAll('.common-chat .message-info-icon');
+
+let chatTabs = document.querySelectorAll('.chat-tab'),
+    welcomeChatHeading = document.querySelector('.welcome-chat .lg-heading'),
+    chatRightColumn = document.querySelector('.chat-right-col'),
+    chatLeftColumn = document.querySelector('.chat-left-col'),
+    sendMessageCol = document.querySelector('.send-messages-col'),
+    chatFooterSubmitCol = document.querySelector('.submit-col'),
+    leftColMessages = document.querySelectorAll('.left-col-message');
+
+let contactHeadCheckBoxes = document.querySelectorAll('.contact-head-label input'),
+    contactHeadSubCheckBoxes = document.querySelectorAll('.parent-checkbox-sub input');
+
+let testChatPrivate = document.querySelector('.chat-1'),
+    testChatCommon = document.querySelector('.chat-2');
+
+let strangeMessages = document.querySelectorAll('.chat-main-message.strange-message');
+let yourMessages = document.querySelectorAll('.chat-main-message.your-message');
+
+/* ЭКРАНЫ В ПРАВОЙ КОЛОНКЕ */
 
 let screens = [welcomeChatScreen, allContactsScreen, newChatScreen, addUsersScreen, chatScreen, privateScreen, commonChatScreen, seenMessagesScreen, commonChatSettingsScreen];
+
+/* Функция для отключения всех экранов,
+   кроме одного по индексу */
+
+   function activeScreen(screens, index){
+    screens.forEach((item)=>{
+        item.classList.remove('active');
+    })  
+    screens[index].classList.add('active');
+}
+
+/* Функция для открытия нужного экрана разработчикам */
+
+function openScreen(screens, index){
+    chatLeftColumn.classList.add('hide-screen');
+    chatRightColumn.classList.remove('hide-screen');
+    activeScreen(screens, index);
+}
+
+openScreen(screens, 7);
+
+/* СООБЩЕНИЯ И УПРАВЛЕНИЕ ИХ ЧЕКБОКСАМИ */
+
+strangeMessages.forEach((message)=>{
+    message.addEventListener('click', ()=>{
+        if(message.querySelector('input').checked === true && !(message.classList.contains('active'))){
+            message.classList.add('active');
+            message.classList.remove('disabled');
+        }
+        else if(!(message.querySelector('input').checked === true) && message.classList.contains('active')){
+            message.classList.remove('active');
+            message.classList.add('disabled');
+        }
+        strangeMessages.forEach((message)=>{
+            if(!(message.classList.contains('active'))){
+                message.classList.add('disabled');
+            }
+        });
+
+        yourMessages.forEach((message)=>{
+            if(!(message.classList.contains('active'))){
+                 message.classList.add('disabled');
+            }
+        });
+    });
+});
+
+yourMessages.forEach((message)=>{
+    message.addEventListener('click', ()=>{
+        if(message.querySelector('input').checked === true && !(message.classList.contains('active'))){
+            message.classList.add('active');
+            message.classList.remove('disabled');
+        }
+        else if(!(message.querySelector('input').checked === true) && message.classList.contains('active')){
+            message.classList.remove('active');
+            message.classList.add('disabled');
+        }
+        strangeMessages.forEach((message)=>{
+            if(!(message.classList.contains('active'))){
+                message.classList.add('disabled');
+            }
+        });
+
+        yourMessages.forEach((message)=>{
+            if(!(message.classList.contains('active'))){
+                 message.classList.add('disabled');
+            }
+        });
+    });
+});
+
+/* Сохранить изменения в настройках общего чата */
 
 pushSettingsBtn.addEventListener('click', ()=>{
     activeScreen(screens, 0);
 })
+
+/* Открыть настройки общего чата */
 
 chatSettingsButtons.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
@@ -40,19 +123,27 @@ chatSettingsButtons.forEach((btn)=>{
     });
 })
 
-testChat.addEventListener('click', ()=>{
+/* Открыть тестовый приватный чат */
+
+testChatPrivate.addEventListener('click', ()=>{
     activeScreen(screens, 5);
 })
+
+/* Открыть тестовый общий чат */
 
 testChatCommon.addEventListener('click', ()=>{
     activeScreen(screens, 6);
 })
+
+/* Открыть информацию о пользователях кто прочитал сообщение в общем чате */
 
 messageInfoButtons.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
         activeScreen(screens, 7);
     })
 });
+
+/* Открыть информацию о пользователях кто прочитал сообщение в общем чате */
 
 showInfoButtons.forEach((item)=>{
     item.addEventListener('click', ()=>{
@@ -61,14 +152,20 @@ showInfoButtons.forEach((item)=>{
     });
 })
 
+/* Фокус для textarea */
+
 sendMessageCol.addEventListener('click', ()=>{
     chatFooterTextarea.focus();
 })
+
+/* Создание чата */
 
 finishCreateChatBtn.addEventListener('click', ()=>{
     activeScreen(screens, 4);
     document.body.style.overflowY = 'hidden';
 });
+
+/* Функция для активного сообщения */
 
 leftColMessages.forEach((message)=>{
     message.addEventListener('click', ()=>{
@@ -83,6 +180,8 @@ leftColMessages.forEach((message)=>{
         }
     });
 })
+
+/* Функция для выделения всех чекбоксов в добавлении контаков */
 
 contactHeadCheckBoxes.forEach((checkbox)=>{
     checkbox.addEventListener('click', ()=>{
@@ -99,6 +198,8 @@ contactHeadCheckBoxes.forEach((checkbox)=>{
     })
 });
 
+/* Функция для выделения всех sub-чекбоксов в добавлении контаков */
+
 contactHeadSubCheckBoxes.forEach((checkbox)=>{
     checkbox.addEventListener('click', ()=>{
         if(checkbox.checked === true){
@@ -114,15 +215,6 @@ contactHeadSubCheckBoxes.forEach((checkbox)=>{
     })
 });
 
-/* Функция для отключения всех экранов,
-   кроме одного по индексу */
-
-function activeScreen(screens, index){
-    screens.forEach((item)=>{
-        item.classList.remove('active');
-    })  
-    screens[index].classList.add('active');
-}
 /* Мои чаты и Отслеживаемые чаты вкладки */
 
 chatTabs.forEach((item)=>{
@@ -149,10 +241,6 @@ chatTabs.forEach((item)=>{
     });
 })
 
-// 1 chatLeftColumn.classList.add('hide-screen');
-// chatRightColumn.classList.remove('hide-screen');
-// activeScreen(screens, 8);
-
 /* Для телефонов стартовый экран */
 
 if(document.documentElement.clientWidth < 768){
@@ -168,8 +256,6 @@ if(document.documentElement.clientWidth < 768){
     });
 
 }
-
-
 
 /* Вкладка все контакты */
 
@@ -226,8 +312,7 @@ addUsersBtn.addEventListener('click', ()=>{
 });
 
 
- /* Показать приватную информацию о пользователе */
-
+ /* Наблюдатель за сменой классов */
 
  let observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
@@ -243,6 +328,7 @@ addUsersBtn.addEventListener('click', ()=>{
 });
 
 // настраиваем наблюдатель
+
 let config = { attributes: true, childList: true, characterData: true }
 
 for(let screen of screens){
